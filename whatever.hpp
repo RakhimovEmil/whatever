ab72ae775f86d18184004603034727ded48b1fe6
RakhimovEmil <69158088+RakhimovEmil@users.noreply.github.com>
2021-12-21 18:39:14 +0300

    Update whatever.hpp

--- whatever.hpp
+++ whatever.hpp
@@ -1,15 +1,62 @@
 #pragma once
 #include <type_traits>
-#include <initializer_list>
 #include <typeinfo>
 #include <memory>
 
 namespace utils {
-
     class whatever {
+    public:
+        whatever() : ptr_(nullptr) {}
+        explicit whatever(std::nullptr_t x) : ptr_(x) {}
 
+        template<typename T>
+        whatever(const T& value) : ptr_(new _whatever<std::decay_t<const T>>(value)) {}
+
+        template<typename T>
+        whatever(T& value) : ptr_(new _whatever<std::decay_t<T>>(value)) {}
+
+        whatever(const whatever& copy) : ptr_(copy.ptr_ == nullptr ? nullptr : copy.ptr_->Copy()) {}
+        whatever(whatever& copy)       : ptr_(copy.ptr_ == nullptr ? nullptr : copy.ptr_->Copy()) {}
+
+        template<typename T>
+        whatever& operator=(T& x) {
+            whatever cur(x);
+            this->swap(cur);
+            return *this;
+        }
+
+        whatever& operator=(const whatever& copy) {
+            if (this == &copy) {
+                return *this;
+            }
+            whatever cur(copy);
+            this->swap(cur);
+            return *this;
+        }
+
+        void clear() {
+            delete ptr_;
+            ptr_ = nullptr;
+        }
+
+        bool empty() const { return ptr_ == nullptr; }
+
+        const std::type_info& cur_type() const { return ptr_ == nullptr ? typeid(void) : ptr_->cur_type(); }
+
+        template<typename T> friend T* whatever_cast(whatever* cur);
+        template<typename T> const T* whatever_cast(const whatever* cur);
+        template<typename T> T whatever_cast(whatever& cur);
+        template<typename T> T whatever_cast(const whatever& cur);
+
+        void swap(whatever& a) { std::swap(ptr_, a.ptr_); }
+
+        ~whatever() {
+            if (ptr_ != nullptr) {
+                delete ptr_;
+                ptr_ = nullptr;
+            }
+        }
     private:
-
         class I_whatever {
         public:
             virtual ~I_whatever(){}
@@ -24,96 +71,12 @@ namespace utils {
 
             explicit _whatever(const T& value) : value_(value) {}
 
-            I_whatever* Copy() const override {
-                return new _whatever<T>(value_);
-            }
+            I_whatever* Copy() const override { return new _whatever<T>(value_); }
 
-            const std::type_info& cur_type() const override {
-                return typeid(value_);
-            }
+            const std::type_info& cur_type() const override { return typeid(value_); }
         };
 
         I_whatever* ptr_ = nullptr;
-
-    public:
-
-        whatever() : ptr_(nullptr) {}
-
-        whatever(std::nullptr_t x) : ptr_(x) {}
-
-        template<typename T>
-        whatever(const T& value) : ptr_(new _whatever<typename std::decay<const T>::type>(value)) {}
-
-        template<typename T>
-        whatever(T& value) : ptr_(new _whatever<typename std::decay<T>::type>(value)) {}
-
-        template<typename T>
-        whatever& operator=(const T& x) {
-            delete ptr_;
-            ptr_ = new _whatever<typename std::decay<const T>::type>(x);
-            return *this;
-        }
-
-        template<typename T>
-        whatever& operator=(T& x) {
-            delete ptr_;
-            ptr_ = new _whatever<typename std::decay<T>::type>(x);
-            return *this;
-        }
-
-        whatever(const whatever& copy) : ptr_(copy.ptr_ == nullptr ? nullptr : copy.ptr_->Copy()) {}
-        whatever(whatever& copy) : ptr_(copy.ptr_ == nullptr ? nullptr : copy.ptr_->Copy()) {}
-        whatever& operator=(whatever& copy) {
-            if (this == &copy) {
-                return *this;
-            }
-            delete ptr_;
-            if (copy.ptr_ != nullptr) {
-                ptr_ = copy.ptr_->Copy();
-            } else {
-                ptr_ = nullptr;
-            }
-            return *this;
-        }
-        whatever& operator=(const whatever& copy) {
-            if (this == &copy) {
-                return *this;
-            }
-            delete ptr_;
-            if (copy.ptr_ != nullptr) {
-                ptr_ = copy.ptr_->Copy();
-            } else {
-                ptr_ = nullptr;
-            }
-            return *this;
-        }
-
-        ~whatever() {
-            if (ptr_ != nullptr) {
-                delete ptr_;
-                ptr_ = nullptr;
-            }
-        }
-
-        void clear() {
-            delete ptr_;
-            ptr_ = nullptr;
-        }
-
-        bool empty() const {
-            return ptr_ == nullptr;
-        }
-
-        const std::type_info& cur_type() const {
-            return ptr_ == nullptr ? typeid(void) : ptr_->cur_type();
-        }
-
-        template<typename T> friend T* whatever_cast(whatever* cur);
-        template<typename T> const T* whatever_cast(const whatever* cur);
-        template<typename T> T whatever_cast(whatever& cur);
-        template<typename T> T whatever_cast(const whatever& cur);
-
-        friend void swap(whatever& a, whatever& b);
     };
 
     class bad_whatever_cast : public std::bad_cast {
@@ -131,7 +94,6 @@ namespace utils {
         if (cur->cur_type() == typeid(T)) {
             return &static_cast<whatever::_whatever<T> *>(cur->ptr_)->value_;
         }
-
         return nullptr;
     }
 
@@ -140,25 +102,21 @@ namespace utils {
         if (cur == nullptr) {
             return nullptr;
         }
-
         return whatever_cast<T>(const_cast<whatever*>(cur));
     }
 
     template<typename T>
     inline T whatever_cast(whatever& cur) {
-        if (!whatever_cast<typename std::remove_reference<T>::type>(&cur)) {
+        if (!whatever_cast<std::remove_reference_t<T>>(&cur)) {
             throw bad_whatever_cast();
         }
-
-        return *whatever_cast<typename std::remove_reference<T>::type>(&cur);
+        return *whatever_cast<std::remove_reference_t<T>>(&cur);
     }
 
     template<typename T>
     inline T whatever_cast(const whatever& cur) {
-        return whatever_cast<const typename std::remove_reference<T>::type&>(const_cast<whatever&>(cur));
+        return whatever_cast<const std::remove_reference_t<T>&>(const_cast<whatever&>(cur));
     }
 
-    inline void swap(whatever& a, whatever& b) {
-        std::swap(a.ptr_, b.ptr_);
-    }
+    inline void swap(whatever& a, whatever& b) { a.swap(b); }
 }
